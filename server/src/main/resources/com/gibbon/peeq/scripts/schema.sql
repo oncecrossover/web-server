da8114e44ddc3c3f49c936e7d2f1a1acfed4d47e
Xiaobing Zhou <xzhou@hortonworks.com>
2016-07-28 20:01:18 -0700

    PEEQ-178. backend: change all schema by using NVARCHAR and CURRENT_TIMESTAMP

--- server/src/main/resources/com/gibbon/peeq/scripts/schema.sql
+++ server/src/main/resources/com/gibbon/peeq/scripts/schema.sql
@@ -1,63 +1,65 @@
 CREATE TABLE `User` (
-  `uid` varchar(200) NOT NULL,
-  `firstName` varchar(100) DEFAULT NULL,
-  `middleName` varchar(100) DEFAULT NULL,
-  `lastName` varchar(100) DEFAULT NULL,
-  `pwd` varchar(100) NOT NULL,
-  `createdTime` datetime DEFAULT NULL,
-  `updatedTime` datetime DEFAULT NULL,
+  `uid` NVARCHAR(200) NOT NULL,
+  `firstName` NVARCHAR(100) DEFAULT NULL,
+  `middleName` NVARCHAR(100) DEFAULT NULL,
+  `lastName` NVARCHAR(100) DEFAULT NULL,
+  `pwd` NVARCHAR(100) NOT NULL,
+  `createdTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `Profile` (
-  `uid` varchar(200) NOT NULL,
-  `avatarUrl` varchar(1000) DEFAULT NULL,
-  `fullName` varchar(500) DEFAULT NULL,
-  `title` varchar(1000) DEFAULT NULL,
-  `aboutMe` varchar(2000) DEFAULT NULL,
+  `uid` NVARCHAR(200) NOT NULL,
+  `avatarUrl` NVARCHAR(1000) DEFAULT NULL,
+  `fullName` NVARCHAR(500) DEFAULT NULL,
+  `title` NVARCHAR(1000) DEFAULT NULL,
+  `aboutMe` NVARCHAR(2000) DEFAULT NULL,
+  `createdTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`uid`),
   FOREIGN KEY fk_user(uid) REFERENCES User(uid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `Quanda` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `asker` varchar(100) NOT NULL,
-  `question` varchar(2000) NOT NULL,
-  `responder` varchar(100) NOT NULL,
-  `answerUrl` varchar(1000) DEFAULT NULL,
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `asker` NVARCHAR(100) NOT NULL,
+  `question` NVARCHAR(2000) NOT NULL,
+  `responder` NVARCHAR(100) NOT NULL,
+  `answerUrl` NVARCHAR(1000) DEFAULT NULL,
   `status` ENUM('PENDING', 'ANSWERED', 'EXPIRED') NOT NULL DEFAULT 'PENDING',
-  `createdTime` datetime NOT NULL,
-  `updatedTime` datetime NOT NULL,
+  `createdTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   FOREIGN KEY fk_asker(asker) REFERENCES User(uid),
   FOREIGN KEY fk_responder(responder) REFERENCES User(uid)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `Snoop` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `uid` varchar(200) NOT NULL,
-  `quandaId` bigint unsigned NOT NULL,
-  `createdTime` datetime NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` NVARCHAR(200) NOT NULL,
+  `quandaId` BIGINT UNSIGNED NOT NULL,
+  `createdTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   FOREIGN KEY fk_user(uid) REFERENCES User(uid),
   FOREIGN KEY fk_quanda(quandaId) REFERENCES Quanda(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `Payment` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `uid` varchar(200) NOT NULL,
-  `accId` varchar(200) NOT NULL,
+  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  `uid` NVARCHAR(200) NOT NULL,
+  `accId` NVARCHAR(200) NOT NULL,
   `type` ENUM('CARD', 'CHECKING') NOT NULL,
-  `lastFour` varchar(10) NOT NULL,
-  `createdTime` datetime DEFAULT NULL,
+  `lastFour` NVARCHAR(10) NOT NULL,
+  `createdTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY(uid, accId),
   FOREIGN KEY fk_user(uid) REFERENCES User(uid)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
 
 CREATE TABLE `Balance` (
-  `uid` varchar(200) NOT NULL,
-  `val` bigint DEFAULT 0,
+  `uid` NVARCHAR(200) NOT NULL,
+  `val` BIGINT DEFAULT 0,
   PRIMARY KEY (`uid`),
   FOREIGN KEY fk_user(uid) REFERENCES User(uid)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
