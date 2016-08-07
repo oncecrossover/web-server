0e86d58a1a64233228b7622142da48f8ecb424ea
Xiaobing Zhou <xzhou@hortonworks.com>
2016-08-06 19:23:06 -0700

    PEEQ-203. backend.payment: implement table schema of QaTransaction

---
+++ server/src/main/java/com/gibbon/peeq/db/model/QaTransaction.java
@@ -0,0 +1,150 @@
+package com.gibbon.peeq.db.model;
+
+import java.io.IOException;
+import java.util.Date;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class QaTransaction {
+  public enum TransType {
+    ASKED, SNOOPED
+  }
+
+  private long id;
+  private String uid;
+  private String type;
+  private long quandaId;
+  private Double amount;
+  private Date createdTime;
+
+  public long getId() {
+    return id;
+  }
+
+  public QaTransaction setId(final long id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getUid() {
+    return uid;
+  }
+
+  public QaTransaction setUid(final String uid) {
+    this.uid = uid;
+    return this;
+  }
+
+  public String getType() {
+    return type;
+  }
+
+  public QaTransaction setType(final String type) {
+    this.type = type;
+    return this;
+  }
+
+  public long getQuandaId() {
+    return quandaId;
+  }
+
+  public QaTransaction setQuandaId(final long quandaId) {
+    this.quandaId = quandaId;
+    return this;
+  }
+
+  public Double getAmount() {
+    return amount;
+  }
+
+  public QaTransaction setAmount(final double amount) {
+    this.amount = amount;
+    return this;
+  }
+
+  public Date getCreatedTime() {
+    return createdTime;
+  }
+
+  public QaTransaction setCreatedTime(final Date createdTime) {
+    this.createdTime = createdTime;
+    return this;
+  }
+
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (obj == null) {
+      return false;
+    }
+
+    if (getClass() == obj.getClass()) {
+      final QaTransaction that = (QaTransaction) obj;
+      if (isEqual(this.getId(), that.getId())
+          && isEqual(this.getUid(), that.getUid())
+          && isEqual(this.getType(), that.getType())
+          && isEqual(this.getQuandaId(), that.getQuandaId())
+          && isEqual(this.getAmount(), that.getAmount())) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private boolean isEqual(Object a, Object b) {
+    return a == null ? b == null : a.equals(b);
+  }
+
+  public QaTransaction setAsIgnoreNull(final QaTransaction that) {
+    if (that == null) {
+      return null;
+    }
+
+    this.setId(that.getId());
+    if (that.getUid() != null) {
+      this.setUid(that.getUid());
+    }
+    if (that.getType() != null) {
+      this.setType(that.getType());
+    }
+    this.setQuandaId(that.getQuandaId());
+    if (that.getAmount() != null) {
+      this.setAmount(that.getAmount());
+    }
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    try {
+      return toJsonStr();
+    } catch (JsonProcessingException e) {
+      return "";
+    }
+  }
+
+  public byte[] toJsonByteArray() throws JsonProcessingException {
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.writeValueAsBytes(this);
+  }
+
+  public String toJsonStr() throws JsonProcessingException {
+    ObjectMapper mapper = new ObjectMapper();
+    return mapper.writeValueAsString(this);
+  }
+
+  public static QaTransaction newInstance(final byte[] json)
+      throws JsonParseException, JsonMappingException, IOException {
+    ObjectMapper mapper = new ObjectMapper();
+    final QaTransaction result = mapper.readValue(json, QaTransaction.class);
+    return result;
+  }
+}
---
+++ server/src/test/java/com/gibbon/peeq/db/model/TestQaTransaction.java
@@ -0,0 +1,144 @@
+package com.gibbon.peeq.db.model;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.IOException;
+import java.util.Random;
+import java.util.UUID;
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.gibbon.peeq.db.model.QaTransaction;
+import com.gibbon.peeq.db.model.QaTransaction.TransType;
+import com.gibbon.peeq.db.util.HibernateTestUtil;
+
+public class TestQaTransaction {
+  private static final Logger LOG = LoggerFactory.getLogger(TestQaTransaction.class);
+  private static Random random = new Random(System.currentTimeMillis());
+
+  @Test(timeout = 60000)
+  public void testRandomTransactionToJason() throws IOException {
+    verifyObjectJason(newRandomInstance());
+  }
+
+  @Test(timeout = 60000)
+  public void testTransactionToJason() throws IOException {
+    verifyObjectJason(newInstance());
+  }
+
+
+  private void verifyObjectJason(Object originalInstance) throws IOException {
+    final ObjectMapper mapper = new ObjectMapper();
+
+    // convert object to json
+    final String originalInstanceJson = mapper.writeValueAsString(originalInstance);
+    LOG.info(originalInstanceJson);
+
+    // convert json to object
+    final QaTransaction newInstance = mapper.readValue(originalInstanceJson, QaTransaction.class);
+    final String newInstanceJson = mapper.writeValueAsString(newInstance);
+    LOG.info(newInstanceJson);
+    assertEquals(originalInstanceJson, newInstanceJson);
+    assertEquals(originalInstance, newInstance);
+  }
+
+  private QaTransaction newRandomInstance() {
+    final QaTransaction result = new QaTransaction();
+    result.setId(random.nextLong())
+          .setUid(UUID.randomUUID().toString())
+          .setType(TransType.ASKED.toString())
+          .setQuandaId(random.nextLong())
+          .setAmount(random.nextDouble());
+    return result;
+  }
+
+  private Object newInstance() {
+    final QaTransaction result = new QaTransaction();
+    result.setId(random.nextLong())
+          .setUid("edmuand")
+          .setType(TransType.ASKED.toString())
+          .setQuandaId(10)
+          .setAmount(1000);
+    return result;
+  }
+
+  @Test(timeout = 60000)
+  public void testCreateRecord() throws JsonProcessingException {
+    Session session = null;
+    Transaction txn = null;
+    QaTransaction retInstance = null;
+
+    /* insert user */
+    final User randomUser = TestUser.insertRandomUser();
+
+    final QaTransaction randomInstance = newRandomInstance();
+    randomInstance.setUid(randomUser.getUid());
+
+    /* insert */
+    session = HibernateTestUtil.getSessionFactory().getCurrentSession();
+    txn = session.beginTransaction();
+    session.save(randomInstance);
+    txn.commit();
+
+    /* query */
+    session = HibernateTestUtil.getSessionFactory().getCurrentSession();
+    txn = session.beginTransaction();
+    retInstance = (QaTransaction) session.get(QaTransaction.class,
+        randomInstance.getId());
+    txn.commit();
+
+    /* verify */
+    assertEquals(randomInstance, retInstance);
+  }
+
+  @Test(timeout = 60000)
+  public void testDeleteRecord() throws JsonProcessingException {
+    Session session = null;
+    Transaction txn = null;
+    QaTransaction retInstance = null;
+
+    /* insert user */
+    final User randomUser = TestUser.insertRandomUser();
+
+    final QaTransaction randomInstance = newRandomInstance();
+    randomInstance.setUid(randomUser.getUid());
+
+    /* insert */
+    session = HibernateTestUtil.getSessionFactory().getCurrentSession();
+    txn = session.beginTransaction();
+    session.save(randomInstance);
+    txn.commit();
+
+    /* query */
+    session = HibernateTestUtil.getSessionFactory().getCurrentSession();
+    txn = session.beginTransaction();
+    retInstance = (QaTransaction) session.get(QaTransaction.class,
+        randomInstance.getId());
+    txn.commit();
+
+    /* verify */
+    assertEquals(randomInstance, retInstance);
+
+    /* delete */
+    session = HibernateTestUtil.getSessionFactory().getCurrentSession();
+    txn = session.beginTransaction();
+    session.delete(retInstance);
+    txn.commit();
+
+    /* query */
+    session = HibernateTestUtil.getSessionFactory().getCurrentSession();
+    txn = session.beginTransaction();
+    retInstance = (QaTransaction) session.get(QaTransaction.class,
+        retInstance.getId());
+    txn.commit();
+
+    /* verify */
+    assertEquals(null, retInstance);
+  }
+}
