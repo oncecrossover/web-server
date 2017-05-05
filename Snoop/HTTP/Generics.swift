7ca7fea53771dba2142b12c6bedc130dac32c148
Bowen Zhang <bowenzhangusa@yahoo.com>
2017-05-04 20:21:10 -0700

    SNOOP-749 frontend.security: frontend probably can't access server through SSL

--- Snoop/HTTP/Generics.swift
+++ Snoop/HTTP/Generics.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import Security
 class Generics: NSObject, URLSessionDelegate {
   var HTTPHOST = Bundle.main.object(forInfoDictionaryKey: "HTTPHOST") as! String
   override init() {
@@ -26,36 +27,90 @@ class Generics: NSObject, URLSessionDelegate {
   }
 
   func urlSession(_ session: URLSession,  didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-    let serverTrust = challenge.protectionSpace.serverTrust
-    let certificate = SecTrustGetCertificateAtIndex(serverTrust!, 0)
+    if (challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodServerTrust) {
+      let serverTrust = challenge.protectionSpace.serverTrust
+      let certificate = SecTrustGetCertificateAtIndex(serverTrust!, 0)
 
-    // Set SSL policies for domain name check
-    let policies = NSMutableArray();
-    policies.add(SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString?)))
-    SecTrustSetPolicies(serverTrust!, policies);
+      // Set SSL policies for domain name check
+      let policies = NSMutableArray();
+      policies.add(SecPolicyCreateSSL(true, (challenge.protectionSpace.host as CFString?)))
+      SecTrustSetPolicies(serverTrust!, policies);
 
-    // Evaluate server certificate
-    /*var result = SecTrustResultType.Invalid
-    SecTrustEvaluate(serverTrust!, &result)
-    let isServerTrusted:Bool = (result == SecTrustResultType.Unspecified || result == SecTrustResultType.Proceed)
-    if (result == SecTrustResultType.RecoverableTrustFailure) {
-      let exception = SecTrustCopyExceptions(serverTrust!)
-      SecTrustSetPolicies(serverTrust!, exception)
-      SecTrustEvaluate(serverTrust!, &result)
-    }
-    print("result is \(result)")**/
+      // Evaluate server certificate
+      /*var result = SecTrustResultType.Invalid
+       SecTrustEvaluate(serverTrust!, &result)
+       let isServerTrusted:Bool = (result == SecTrustResultType.Unspecified || result == SecTrustResultType.Proceed)
+       if (result == SecTrustResultType.RecoverableTrustFailure) {
+       let exception = SecTrustCopyExceptions(serverTrust!)
+       SecTrustSetPolicies(serverTrust!, exception)
+       SecTrustEvaluate(serverTrust!, &result)
+       }
+       print("result is \(result)")**/
 
-    // Get local and remote cert data
-    let remoteCertificateData:Data = SecCertificateCopyData(certificate!) as Data
-    let pathToCert = Bundle.main.path(forResource: "snoop-server", ofType: "der")
-    let localCertificate:Data = try! Data(contentsOf: URL(fileURLWithPath: pathToCert!))
+      // Get local and remote cert data
+      let remoteCertificateData:Data = SecCertificateCopyData(certificate!) as Data
+      let pathToCert = Bundle.main.path(forResource: "snoop-server", ofType: "der")
+      let localCertificate:Data = try! Data(contentsOf: URL(fileURLWithPath: pathToCert!))
 
-    if (remoteCertificateData == localCertificate) {
+      if (remoteCertificateData == localCertificate) {
         let credential:URLCredential = URLCredential(trust: serverTrust!)
         completionHandler(.useCredential, credential)
-    } else {
+      } else {
         completionHandler(.cancelAuthenticationChallenge, nil)
+      }
     }
+    else if (challenge.protectionSpace.authenticationMethod == NSURLAuthenticationMethodClientCertificate) {
+      let identityAndTrust:IdentityAndTrust = self.extractIdentity();
+
+      let urlCredential:URLCredential = URLCredential(
+        identity: identityAndTrust.identityRef,
+        certificates: identityAndTrust.certArray as? [AnyObject],
+        persistence: URLCredential.Persistence.forSession);
+
+      completionHandler(URLSession.AuthChallengeDisposition.useCredential, urlCredential);
+    }
+  }
+
+  struct IdentityAndTrust {
+
+    var identityRef:SecIdentity
+    var trust:SecTrust
+    var certArray:AnyObject
+  }
+
+  func extractIdentity() -> IdentityAndTrust {
+    var identityAndTrust:IdentityAndTrust!
+    var securityError:OSStatus = errSecSuccess
+
+    let path = Bundle.main.path(forResource: "snoop-client", ofType: "p12")
+    let PKCS12Data = NSData(contentsOf: URL(fileURLWithPath: path!))!
+    let key : NSString = kSecImportExportPassphrase as NSString
+    let options : NSDictionary = [key : "changeme"]
+    //create variable for holding security information
+    //var privateKeyRef: SecKeyRef? = nil
+
+    var items : CFArray?
+
+    securityError = SecPKCS12Import(PKCS12Data, options, &items)
+
+    if securityError == errSecSuccess {
+      let certItems:CFArray = items as CFArray!;
+      let certItemsArray:Array = certItems as Array
+      let dict:AnyObject? = certItemsArray.first;
+      if let certEntry:Dictionary = dict as? Dictionary<String, AnyObject> {
+
+        // grab the identity
+        let identityPointer:AnyObject? = certEntry["identity"];
+        let secIdentityRef:SecIdentity = identityPointer as! SecIdentity!;
+        // grab the trust
+        let trustPointer:AnyObject? = certEntry["trust"];
+        let trustRef:SecTrust = trustPointer as! SecTrust;
+        // grab the cert
+        let chainPointer:AnyObject? = certEntry["chain"];
+        identityAndTrust = IdentityAndTrust(identityRef: secIdentityRef, trust: trustRef, certArray:  chainPointer!);
+      }
+    }
+    return identityAndTrust;
   }
 
   func createObject(_ URI: String, jsonData: [String:AnyObject], completion: @escaping (String) -> ()) {
